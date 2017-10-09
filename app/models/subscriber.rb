134142e7c25a2820c54d14d9ccdbac2f1e0ef0ba
vmanamino <vmanamino@gmail.com>
2017-10-09 11:28:27 +0000

    descendants

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -33,8 +33,11 @@ class Subscriber < ActiveRecord::Base
             puts "lineage #{lineage}"
             # listings exist for given category
             # creates a separate notification for each book that is cross listed
+            # example is subscriber codes ab ef book3 ab ef, book 3 will appear twice
+            # in separate notifications
             if Listing.find_by category_id: c.id
-                book_ids = Listing.where(category_id: c.id).map(&:book_id)
+                # book_ids = Listing.where(category_id: c.id).map(&:book_id)
+                book_ids = c.listings.map(&:book_id)
                 book_ids.uniq!
                 puts "book ids #{book_ids}"
                 puts "code #{c.code}"
@@ -44,47 +47,85 @@ class Subscriber < ActiveRecord::Base
                     book = Book.find(book_id)
                     puts "book #{book.title}"
                     bn.book = book.title
-                    cat_ids = Listing.where(book_id: book_id).map(&:category_id)
-                    cat_ids.uniq!
+                    # cat_ids = Listing.where(book_id: book_id).map(&:category_id)
+                    # cat_ids.uniq!
                     cat_paths = []
-                    cat_ids.each do |cat_id|
-                        path = []
-                        cat = Category.find(cat_id)
-                        path.push(cat.title)
-                        cat_paths.push(path)
-                    end
+                    # cat_ids.each do |cat_id|
+                    path = []
+                    cat = Category.find(c.id)
+                    path.push(cat.title)
+                    cat_paths.push(path)
+                    # end
                     bn.categoryPaths = cat_paths
                     bn.newsletter = n
                     bn.save
                     bn_objs.push(BookNotificationSerializer.new(bn).to_json)
                 end
-            end
-            if c.descendants.size != 0
-                puts "my desc #{c.descendants.size}"
-                descendant_objs = c.descendants
-                puts "my descc objs #{descendant_objs.map(&:title)}"
-                c.descendants.each do |cat|
-                    puts "cat id #{cat.id}"
-                    if Listing.find_by category_id: cat.id
-                        listing = Listing.find_by category_id: cat.id
-                        puts "listing for desc #{listing.category_id}"
-                        if Book.find_by id: listing.book_id
-                            book = Book.find_by id: listing.book_id
-                            puts "book #{book.title}"
-                            book_cat_ids = Listing.where(book_id: book).map(&:category_id)
-                            puts "book categories #{book_cat_ids}"
-                            bn = BookNotification.new()
-                            cat_paths = []
-                            # book_cat_ids.each do |cat_id|
-                            #     ancestors = cat.ancestors.map(&:title)
-                            #     cat_path = ancestors.drop(lineage)
-                            #     cat_paths.push(cat_path)
-                            # end
-                            # puts "cat paths #{cat_paths}"
+                if c.descendants.size != 0
+                    c.descendants.map(&:id).each do |cat_id|
+                      puts "dec cat id #{cat_id}"
+                      if Listing.find_by category_id: cat_id
+                            book_ids = Listing.where(category_id: cat_id).map(&:book_id)
+                            book_ids.uniq!
+                            puts "book ids #{book_ids}"
+                            puts "code #{c.code}"
+                            puts " code id #{c.id}"
+                            book_ids.each do |book_id|
+                                bn = BookNotification.new()
+                                book = Book.find(book_id)
+                                puts "book #{book.title}"
+                                bn.book = book.title
+                                cat_ids = Listing.where(book_id: book_id).map(&:category_id)
+                                cat_ids.uniq!
+                                cat_paths = []
+                                cat_ids.each do |cat_id|
+                                    path = []
+                                    cat = Category.find(cat_id)
+                                    ancestors = cat.ancestors
+                                        if ancestors.length != 0 or ancestors.length != 1
+                                            subscriber_interests = ancestors.drop(lineage)
+                                            subscriber_interests.map(&:title).each do |cat|
+                                                path.push(cat) 
+                                            end
+                                        end
+                                    path.push(cat.title)
+                                    cat_paths.push(path)
+                                end
+                                bn.categoryPaths = cat_paths
+                                bn.newsletter = n
+                                bn.save
+                                bn_objs.push(BookNotificationSerializer.new(bn).to_json)
+                            end
                         end
                     end
                 end
             end
+            # if c.descendants.size != 0
+            #     puts "my desc #{c.descendants.size}"
+            #     descendant_objs = c.descendants
+            #     puts "my descc objs #{descendant_objs.map(&:title)}"
+            #     c.descendants.each do |cat|
+            #         puts "cat id #{cat.id}"
+            #         if Listing.find_by category_id: cat.id
+            #             listing = Listing.find_by category_id: cat.id
+            #             puts "listing for desc #{listing.category_id}"
+            #             if Book.find_by id: listing.book_id
+            #                 book = Book.find_by id: listing.book_id
+            #                 puts "book #{book.title}"
+            #                 book_cat_ids = Listing.where(book_id: book).map(&:category_id)
+            #                 puts "book categories #{book_cat_ids}"
+            #                 bn = BookNotification.new()
+            #                 cat_paths = []
+            #                 # book_cat_ids.each do |cat_id|
+            #                 #     ancestors = cat.ancestors.map(&:title)
+            #                 #     cat_path = ancestors.drop(lineage)
+            #                 #     cat_paths.push(cat_path)
+            #                 # end
+            #                 # puts "cat paths #{cat_paths}"
+            #             end
+            #         end
+            #     end
+            # end
         #     if c.descendants.length != 0
         #         # length = descendant_objs.length
         #         c.descendants.each do |cat|
